12f191d7fee263db0444f419d2f5c2afadad4754
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-08-30 23:25:33 +0530

    length

--- pages/inventory/index.js
+++ pages/inventory/index.js
@@ -24,7 +24,7 @@ function Inventory({ products }) {
   return (
     <SideLayout>
       <div className="px-6 py-10">
-        <h1 className="text-xl font-semibold">162 Products</h1>
+        <h1 className="text-xl font-semibold">{products.length} Products</h1>
         <div className="flex items-center mt-5 space-x-6">
           <div className="h-12 bg-white rounded px-5 flex items-center w-full">
             <span className="shrink-0">
